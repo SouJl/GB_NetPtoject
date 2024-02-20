7b1dd3be4fa207b92a4a194088bd34515941aaf1
SouJl <meln.alex97@gmail.com>
2024-02-20 12:34:25 +0700

    Implement gate logic

---
+++ Assets/BTA_ProjectData/Scripts/Tools/CollisionDetector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Tools
+{
+    public class CollisionDetector : MonoBehaviour
+    {
+        public event Action<Collider> OnEnter;
+        public event Action<Collider> OnExit;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            OnEnter?.Invoke(other);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            OnExit?.Invoke(other);
+        }
+    }
+}
---
+++ Assets/BTA_ProjectData/Scripts/Tools/GateController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Tools
+{
+    public enum GateState
+    {
+        None,
+        Open,
+        Close,
+    }
+
+    public class GateController : MonoBehaviour
+    {
+        private readonly string StateTrigger = "IsOpen";
+
+        [SerializeField]
+        private LayerMask _whoCanInteract;
+        [SerializeField]
+        private Animator _leftDoorAnimator;
+        [SerializeField]
+        private Animator _rightDoorAnimator;
+        [SerializeField]
+        private CollisionDetector _collisionDetector;
+
+        private GateState _state;
+        private bool _isOppenerExist;
+
+        private void Awake()
+        {
+
+            _isOppenerExist = false;
+            
+            _state = GateState.Close;
+        }
+
+        private void Start()
+        {
+            _collisionDetector.OnEnter += CheckForOpener;
+            _collisionDetector.OnExit += OpenerLeftFromGate;
+        }
+
+        private void Update()
+        {
+            if (!_isOppenerExist)
+                return;
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Open();
+            }
+        }
+
+
+        private void CheckForOpener(Collider collider)
+        {
+            if ((_whoCanInteract & 1 << collider.gameObject.layer) == 1 << collider.gameObject.layer)
+            {
+                _isOppenerExist = true;
+            }
+        }
+        
+        private void OpenerLeftFromGate(Collider collider)
+        {
+            if (!_isOppenerExist)
+                return;
+
+            if ((_whoCanInteract & 1 << collider.gameObject.layer) == 1 << collider.gameObject.layer)
+            {
+                Close();
+            }
+        }
+
+        private void Open()
+        {
+            if (_state == GateState.Open)
+                return;
+
+            _leftDoorAnimator.SetBool(StateTrigger, true);
+            _rightDoorAnimator.SetBool(StateTrigger, true);
+
+            _state = GateState.Open;
+        }
+
+        private void Close()
+        {
+            if (_state == GateState.Close)
+                return;
+
+            _leftDoorAnimator.SetBool(StateTrigger, false);
+            _rightDoorAnimator.SetBool(StateTrigger, false);
+
+            _state = GateState.Close;
+        }
+
+
+        private void OnDestroy()
+        {
+            _collisionDetector.OnEnter -= CheckForOpener;
+        }
+    }
+}
